a3c3f89e92c1fc26d1870f738b63d5c876102f99
李梦超 <lmc01564032@antgroup.com>
2022-12-09 13:49:16 +0800

    09

--- test.js
+++ test.js
@@ -1,36 +1,17 @@
 async function loadNovel() {
   const url = "https://duyi-static.oss-cn-beijing.aliyuncs.com/files/novel.txt";
   const resp = await fetch(url);
-  const reader = resp.body.getReader();
-  const decoder = new TextDecoder();
-  for (;;) {
-    const { value, done } = await reader.read();
-    if (done) {
-      break;
-    }
-    const text = decoder.decode(value);
-    // console.log(text);
-  }
+  const text = await resp.text()
+  console.log(text);
+  // const reader = resp.body.getReader();
+  // const decoder = new TextDecoder();
+  // for (;;) {
+  //   const { value, done } = await reader.read();
+  //   if (done) {
+  //     break;
+  //   }
+  //   const text = decoder.decode(value);
+  //   console.log(text);
+  // }
 }
 // loadNovel();
-
-//1.实例化请求对象
-const url = "https://duyi-static.oss-cn-beijing.aliyuncs.com/files/novel.txt";
-const http = new XMLHttpRequest();
-http.open('get', url);
-http.onreadystatechange = function() {
-    if(http.readyState === 4) {
-        if (http.status === 200 || http.status === 304) {
-            const result = http.responseText
-            // console.log(result);
-            console.log(1);
-            // document.body.innerText = result
-        }
-    }
-}
-http.send()
-const ajax = (url, methods = 'get', params = {}) => {
-    const xhr = new XMLHttpRequest();
-    http.open();
-    
-}
